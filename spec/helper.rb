827af248aa63931e0b3710be46a6b25894377bf2
Dan Melton <melton.dan@gmail.com>
2011-04-16 16:31:17 -0700

    Changed spec file

--- spec/helper.rb
+++ spec/helper.rb
@@ -2,3 +2,18 @@ require 'simplecov'
 SimpleCov.start
 require File.expand_path('../../lib/fcc_reboot', __FILE__)
 require 'rspec'
+require 'webmock'
+
+require 'fcc_reboot'
+
+RSpec.configure do |config|
+  config.include WebMock::API
+end
+
+def fixture_path
+  File.expand_path("../fixtures", __FILE__)
+end
+
+def fixture(file)
+  File.new(fixture_path + '/' + file)
+end
