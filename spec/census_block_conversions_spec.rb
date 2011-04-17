e6c873fcdf46ac40fe4a68aa313e494ca9044374
Javier Muniz <javier@granicus.com>
2011-04-16 18:02:15 -0700

    fixed FindCensusBlock

--- spec/census_block_conversions_spec.rb
+++ spec/census_block_conversions_spec.rb
@@ -9,7 +9,7 @@ describe FccReboot, ".census_block" do
 
   it "should request the correct resource" do
     client = FccReboot.client
-    client.CensusBlock(:latitude => '38.0', :longitude => '-77.5')
+    client.FindCensusBlock(:latitude => '38.0', :longitude => '-77.5')
     a_request(:get, 'http://data.fcc.gov/api/block/find').
       with(:query => {:latitude => '38.0', :longitude => '-77.5'}).
       should have_been_made
@@ -17,7 +17,7 @@ describe FccReboot, ".census_block" do
 
   it "should return the correct results" do
     client = FccReboot.client
-    services = client.CensusBlock(:latitude => '38.0', :longitude => '-77.5')
+    services = client.FindCensusBlock(:latitude => '38.0', :longitude => '-77.5')
     services.should be_a Hash
     services["Block"]["FIPS"].should == '510339905003000'
   end
