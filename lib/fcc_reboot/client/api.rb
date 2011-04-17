fd3f7475abde9b03239d096fc1c3ac9091e440a8
Javier Muniz <javier@granicus.com>
2011-04-16 19:00:54 -0700

    updated documentation

--- lib/fcc_reboot/client/api.rb
+++ lib/fcc_reboot/client/api.rb
@@ -17,12 +17,33 @@ module FccReboot
         JSON.parse(response)["SpeedTestCounty"]
       end
       
+      # This API returns the US Census Bureau Census Block number (aka the 15 character FIPS Code) given 
+      # a passed Latitude and Longitude. The API also returns the US State and County name associated with 
+      # the Block.
+      #
+      # @format :json
+      # @key false
+      # @param options [Hash] A customizable set of options.
+      # @return [Hash]
+      # @see http://reboot.fcc.gov/developer/census-block-conversions-api
+      # @example Provide census block info for the given latitude and longitude
+      #   FccReboot.find_census_block({:latitude => '38.0', :longitude => '-77.5'})
       def find_census_block(options={})
         options.merge({:format => "json"})
         response = get('block/find', options)
         JSON.parse(response)
       end
 
+      # This API returns returns all the broadband providers in a given state, where those operators
+      # or do not operate in multiple states.
+      #
+      # @format :json
+      # @key false
+      # @param options [Hash] A customizable set of options.
+      # @return [Hash]
+      # @see http://reboot.fcc.gov/developer/frn-conversions-api
+      # @example Provide FRN list for the state of Illinois
+      #   FccReboot.frn_getlist({:stateCode => 'IL', :multi => 'No'})
       def frn_getlist(options={})
         options[:multi] = options[:multi] ? "Yes" : "No"
         options.merge({:format => "json"})
@@ -30,6 +51,15 @@ module FccReboot
         JSON.parse(response)
       end
       
+      # This API returns information about a provider by FRN number.
+      #
+      # @format :json
+      # @key false
+      # @param options [Hash] A customizable set of options.
+      # @return [Hash]
+      # @see http://reboot.fcc.gov/developer/frn-conversions-api
+      # @example Provide FRN info for Cygnus Telecommunications Corporation (FRN number 0017855545)
+      #   FccReboot.find_census_block({:frn => '0017855545'})
       def frn_getinfo(options={})
         options.merge({:format => "json"})
         response = get('frn/getInfo', options)
